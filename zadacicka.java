95c1342aff4c88aa0c27692a7b21c9c8330e0db6
Marija Markoska <marija@hotmail.com>
2018-03-22 17:13:38 +0100

    jas sum srcka

--- zadacicka.java
+++ zadacicka.java
@@ -3,6 +3,9 @@ public class zadacicka {
 
 	public static void main(String[] args) {
 		System.out.println("I'm hungry");
+		
+		System.out.println("Jas sum srcka");
+		System.out.println("I kate taka misli");
 
 	}
 
